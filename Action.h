285275f2dbc3d4c4a36d25512fd883bd4cdca279
fEst1ck <wswzk864864@gmail.com>
2020-12-04 01:00:10 -0500

    implement the abstract Action class

---
+++ Action.h
@@ -0,0 +1,19 @@
+//
+// Created by 王泽堃 on 2020-11-25.
+//
+
+#ifndef VM_ACTION_H
+#define VM_ACTION_H
+
+
+#include "State.h"
+
+class Action {
+public:
+    virtual ~Action() = default;
+    virtual void operator()(State &s) = 0;
+    virtual void undo(State &s) = 0;
+};
+
+
+#endif //VM_ACTION_H
